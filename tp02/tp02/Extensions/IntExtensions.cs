46d5243fa673b659700f3e645e8a939b40a11070
Fernando <fdbartoli91@gmail.com>
2022-05-31 01:59:00 -0300

    Se crea el metodo de extensión para el punto 1.
    Metodo de extension INT que divide cualquier número por 0, se captura la excepción, se muestra el mensaje y se muestra un mensaje de fin de operación.

---
+++ tp02/tp02/Extensions/IntExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tp02.Extensions
+{
+    public static class IntExtensions
+    {
+
+        public static int DivideByZero(this int number)
+        {
+            return number / 0;       
+
+        }       
+
+    }
+} 
---
+++ tp02/tp02/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tp02.Extensions;
+
+namespace tp02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 10;
+            try
+            {
+                Console.WriteLine(number.DivideByZero());
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Fin de la operación.");
+            }
+            Console.ReadKey();
+            
+            
+
+        }
+    }
+}
